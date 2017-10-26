2ceb657b5d60285a24053fb339dd79008ce20361
ng0227 <namangupta0227@gmail.com>
2017-10-26 15:12:32 +0530

    setting up mvc structure

--- app/src/main/java/com/example/naman/nutritionchoice/activity/IntroActivity.java
+++ app/src/main/java/com/example/naman/nutritionchoice/activity/IntroActivity.java
@@ -1,13 +1,18 @@
-package com.example.naman.nutritionchoice;
+package com.example.naman.nutritionchoice.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.naman.nutritionchoice.R;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: introduction page max 2 screens
+
     }
 }
